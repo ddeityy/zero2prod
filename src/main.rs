65637e3ced8062ab9632c8f37706a63069d12d30
ddeityy <ddeityy@gmail.com>
2023-06-30 17:41:59 +0000

    4.5.15

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use zero2prod::config::get_configuration;
@@ -10,9 +11,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Could not connect to postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Could not connect to postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
